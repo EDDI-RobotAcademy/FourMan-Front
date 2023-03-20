20bdc6fc1d5be5f9ce0f2479e8fb00aa1464653f
Cong0327 <hsp400@gmail.com>
2023-03-20 21:23:45 +0900

    feat:[박현수] 메인페이지 Navbar UI 배치 [CAFE-RESERVE-FRONTEND-02]

---
+++ frontend/src/components/navigation/NavigationMenuForm.vue
@@ -0,0 +1,107 @@
+<template>
+  <nav>
+    <v-app-bar color="dark" class="flex-grow-0" app dark>
+      <v-app-bar-nav-icon @click="navigation_drawer = !navigation_drawer"/>
+          <router-link to="/">
+            <v-img class="mx-2" src="@/assets/logo/images.png"
+                  max-height="40" max-width="40" contain/>
+          </router-link>
+      <v-toolbar-title class="text--darken-4">
+        <span>카페 예약 페이지</span>
+      </v-toolbar-title>
+
+      <v-row>
+        <v-col>
+          <v-btn>button 1</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn>button 2</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn>button 3</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn>button 4</v-btn>
+        </v-col>
+
+      </v-row>
+
+
+      <v-btn v-if="isAuthenticated == true" text color="grey" v-on:click="resign">
+        <span>회원 탈퇴</span>
+        <v-icon right>mdi-login</v-icon>
+      </v-btn>
+      <v-btn text color="grey" onclick="location.href='/'">
+        <span>Sign Up</span>
+        <v-icon right>mdi-account-plus-outline</v-icon>
+      </v-btn>
+      <v-btn v-if="isAuthenticated == false" text color="grey" onclick="location.href='/'">
+        <span>Sign In</span>
+        <v-icon right>mdi-login</v-icon>
+      </v-btn>
+      <v-btn v-else text color="grey" v-on:click="logout">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="navigation_drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">닉네임</v-list-item-title>
+          <router-link to="MemberMypage">
+          <v-list-item-subtitle>Mypage</v-list-item-subtitle>
+          </router-link>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item v-for="link in links" :key="link.name" router :to="link.route">
+          <v-list-item-action>
+            <v-icon left>
+              {{ link.icon }}
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+
+import {mapState} from "vuex";
+
+export default {
+  name: 'NavigationMenuForm',
+  data () {
+    return {
+      navigation_drawer: false,
+      links: [
+      { icon: 'mdi-bulletin-board', text: '게시판', name: 'Board', route: '/board' },
+          { icon: 'mdi-coffee', text: '카페 목록',
+              name: 'CafeListPage', route: '/cafe-list-page' },
+      ]
+  }
+},
+computed: {
+    ...mapState(["isAuthenticated"]),
+  },
+  mounted() {
+    if (localStorage.getItem("userInfo")) {
+      this.$store.state.isAuthenticated = true;
+    } else {
+      this.$store.state.isAuthenticated = false;
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
