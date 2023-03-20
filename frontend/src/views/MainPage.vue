c8b3728ad9c2e28fb8f679292417fc45a80349cb
Cong0327 <hsp400@gmail.com>
2023-03-20 21:24:28 +0900

    feat:[박현수] 메인페이지 크로셀 배치[CAFE-RESERVE-FRONTEND-02]

---
+++ frontend/src/views/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <MainCarouselForm></MainCarouselForm>
+</template>
+
+<script>
+  import MainCarouselForm from '@/components/main/MainCarouselForm.vue';
+
+  export default {
+    name: 'MainPage',
+
+    components: {
+      MainCarouselForm
+    },
+  }
+</script>
