54a03a6affac5eb36fa25e0f769f0ced6df66b33
Cong0327 <hsp400@gmail.com>
2023-03-21 20:29:43 +0900

    feat:[박현수] 메인페이지 navbar, carousel,footer[CAFE-RESERVE-FRONTEND-02]

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <v-app>
-     <router-view/>
+    <navigation-menu-form/>
+    <v-main>
+      <router-view/>
+    </v-main>
+    <main-footer-form/>
   </v-app>
 </template>
 
 <script>
+
+import NavigationMenuForm from '@/components/navigation/NavigationMenuForm.vue';
+import MainFooterForm from '@/components/footer/MainFooterForm.vue';
 
 export default {
   name: 'App',
@@ -12,8 +19,10 @@ export default {
   data: () => ({
     //
   }),
-  components:{
-    
-  }
+
+  components: {
+    NavigationMenuForm,
+    MainFooterForm
+  },
 };
 </script>
