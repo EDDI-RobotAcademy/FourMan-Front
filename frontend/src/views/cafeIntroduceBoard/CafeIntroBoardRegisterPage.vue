1b576ebd6d590989af89d527c9de9604d07b9436
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-03-26 04:47:05 +0900

    feat: [김영진] 카페소개게시판에서 카페등록을 눌렀을 때 나오는 카페 등록 페이지[CAFE-RESERVE-FRONT-31]

---
+++ frontend/src/views/cafeIntroduceBoard/CafeIntroBoardRegisterPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container class="detailWrap">
+    <h2>카페 등록</h2>
+    <CafeIntroBoardRegisterForm class="mt-15"/>
+    <!-- <CafeIntroBoardRegisterForm @submit="onSubmit"/> -->
+  </v-container>
+</template>
+
+<script>
+import CafeIntroBoardRegisterForm from '@/components/cafeIntroduceBoard/CafeIntroBoardRegisterForm.vue'
+// import { mapActions } from 'vuex'
+
+export default {
+    components: { CafeIntroBoardRegisterForm },
+    name: "CafeIntroBoardRegisterPage",
+    methods: {
+        // ...mapActions ([
+        //     'requestCreateCafeIntroToSpring'
+        // ]),
+        // async onSubmit (payload) { //formData도착
+        //     await this.requestCreateCafeIntrToSpring(payload)
+        //     await this.$router.push({
+        //         name: 'CafeIntroBoardListPage'
+        //     })
+        // }
+    }
+}
+
+</script>
+
+<style scoped>
+.detailWrap {
+  width: 1200px;
+  margin: 0 auto;
+  padding: 40px 0;
+}
+</style>
