57cfbfddb04470677dfe58b410643c6881ae7ca2
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-03-28 16:05:14 +0900

    test: [김영진] 카페등록시 업로드되는 이미지파일 추가

--- frontend/src/components/cafeIntroduceBoard/CafeIntroBoardRegisterForm.vue
+++ frontend/src/components/cafeIntroduceBoard/CafeIntroBoardRegisterForm.vue
@@ -317,7 +317,7 @@ export default {
             alert(res.message);
           });
 
-        this.$router.push({ name: "CafeIntroBoardListPage" });
+        await this.$router.push({ name: "CafeIntroBoardListPage" });
         //파일 업로드 하지 않은 경우
       } else {
         alert("상품 사진을 업로드해주세요");
