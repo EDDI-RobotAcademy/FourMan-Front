6e8700215d257f33987084cb0b3b89fa78fc961e
MDandersonm <1215kimyj@gmail.com>
2023-03-29 21:24:07 +0900

    upgrade:[김영진] footer ui 예쁘게 업그레이드 [CAFE-RESERVE-FRONT-11]

--- frontend/src/components/footer/MainFooterForm.vue
+++ frontend/src/components/footer/MainFooterForm.vue
@@ -1,41 +1,82 @@
 <template>
-    <v-footer inset padless class="justify-center pl-0">
-      <v-card flat class="text-center" width="100%">
-        <v-card-text>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
-        <v-card-text class="pt-0">
-          카페 예약 페이지(추후 변경)
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-text class="blue--text">
-          <strong>-KH정보교육원 5조-<br>
-            팀장: 김영진<br>
-            팀원: 박현우, 정명진, 박현수 </strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
-  </template>
+  <v-footer inset padless class="justify-center pl-0">
+    <v-card flat class="text-center white--text" width="100%" color="brown lighten-1">
+      <v-divider></v-divider>
+      <v-container style="width: 1000px">
+        <v-layout>
+          <v-layout style="margin-top: 30px; margin-bottom: 50px">
+            <div style="width: 130px; height: 130px">
+              <v-img src="@/assets/logo.png"> </v-img>
+            </div>
+
+            <!--바이디어 정보 -->
+            <div style="margin-left: 50px">
+              <h4 style="margin-bottom: 10px">(주)FOURMAN</h4>
+              <div v-for="(item, index) in buy_diaInfo" :key="index">
+                <h5>팀장: {{ item.teamJang }}</h5>
+                <h5>조원: {{ item.name }}</h5>
+                <h5>주소: {{ item.address }}</h5>
+                <h5>사업자등록번호:{{ item.companyNumber }}</h5>
+                <h5>통신판매업: {{ item.mailOrderBusiness }}</h5>
+                <h5>팩스: {{ item.faxNum }}</h5>
+              </div>
+              <h5 style="margin-top: 40px">
+                Copyright © 2023 Backpackr All right reserved.
+              </h5>
+            </div>
+          </v-layout>
+
+          <v-layout
+            style="margin-top: 30px; margin-bottom: 50px; margin-left: 100px"
+          >
+            <div>
+              <h3>고객센터</h3>
+
+              <h4 style="margin-top: 10px">
+                1664-1234
+                <span style="font-size: 15px;"
+                  >월-금  9:00 ~18:00</span                >
+              </h4>
+              <div style="margin-top: 12px">
+                <h5>메일:&nbsp; help@fourman.com</h5>
+                <h5>제휴문의:&nbsp; partnership@fourman.com</h5>
+              </div>
+
+              <v-btn
+                style="margin-top: 20px"
+                v-for="icon in icons"
+                :key="icon"
+                icon
+              >
+                <v-icon size="24" color="white">
+                  {{ icon }}
+                </v-icon>
+              </v-btn>
+            </div>
+          </v-layout>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-footer>
+</template>
 
   <script>
-
-  export default {
-        name: "MainFooterForm",
-        data: () => ({
-            icons: [
-                "mdi-facebook",
-                "mdi-twitter",
-                "mdi-instagram"
-            ]
-        })
-  }
-
-  </script>
+export default {
+  name: "MainFooterForm",
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-instagram"],
+    buy_diaInfo: [
+      { teamJang: "김영진",
+        name: "정명진,박현수,박현우",
+        address: "서울특별시 강남구 강남구 테헤란로14길 6",
+        companyNumber: "123-45-67890",
+        mailOrderBusiness: "2022-fourman-1234",
+        faxNum: "070-1234-5678",
+      },
+    ],
+  }),
+};
+</script>
 
   <style>
-
-  </style>
+</style>
