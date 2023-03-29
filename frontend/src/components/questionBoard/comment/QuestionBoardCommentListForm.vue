8298c73cf26c26e633fc94814d80de9266d005e7
Cong0327 <hsp400@gmail.com>
2023-03-30 03:22:05 +0900

    feat:[박현수] 질문게시판 읽기 페이지에서 댓글 삭제하는 UI[CAFE-RESERVE-FRONT-46]

--- frontend/src/components/questionBoard/comment/QuestionBoardCommentListForm.vue
+++ frontend/src/components/questionBoard/comment/QuestionBoardCommentListForm.vue
@@ -3,7 +3,14 @@
     <h4>댓글 : {{ commentSize }} 개</h4>
     <ul>
       <li v-for="comment in comments" :key="comment.commentId">
-        <div> {{ comment.commentWriter }} |  {{ comment.comment }}</div>
+        <div> {{ comment.commentWriter }} |  {{ comment.comment }}
+          <v-btn class="small-button" icon>
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+          <v-btn class="small-button" @click="deleteComment(comment.commentId)" icon>
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </div>
 
       </li>
     </ul>
@@ -12,6 +19,8 @@
 </template>
 
 <script>
+
+import {mapActions} from 'vuex'
 
 export default {
     name: "QuestionBoardCommentListForm",
@@ -28,9 +37,32 @@ export default {
     props : {
         comments : {
             type: Array,
-
+        },
+        comment: {
+          type: Object
         }
+    },
+    methods: {
+      ...mapActions([
+      'requestQuestionBoardCommentDeleteToSpring',
+      ]),
+
+      async deleteComment(commentId) {
+        console.log('commentid : ' + commentId)
+        await this.requestQuestionBoardCommentDeleteToSpring(commentId)
+        await this.$router.go((this.$router.currentRoute))
+    }
+      }
     }
 
-}
+
 </script>
+
+<style scoped>
+.small-button {
+  width: 5px;
+  height: 5px;
+  padding-right: 11px;
+  padding-left: 15px;
+}
+</style>
