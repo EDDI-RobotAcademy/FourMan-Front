096e2e19fefa4d5c8c5a435ebccd10c65a30c687
jmj30040 <jmj30040@gmail.com>
2023-03-30 16:55:24 +0900

    add: [정명진] 리뷰 게시판 리스트 페이지네이션 추가 [CAFE-RESERVE-FRONT-37]

--- frontend/src/components/reviewBoard/ReviewBoardListForm.vue
+++ frontend/src/components/reviewBoard/ReviewBoardListForm.vue
@@ -1,50 +1,70 @@
 <template>
-    <div class="main-container">
-        <v-container>
-          <v-row>
-            <v-col
-              v-for="reviewBoard in reversedPosts"
-              :key="reviewBoard.reviewBoardId"
-              cols="8"
-              sm="3"
-              md="3"
-              >
-                <v-card class="text-center" :to="{ name: 'ReviewBoardReadPage', params: { reviewBoardId: reviewBoard.reviewBoardId.toString() }}">
-                <v-img
-                  :src="reviewBoard.firstPhoto ? getImagePath(reviewBoard.firstPhoto) : ''"
-                  aspect-ratio="1"
-                  class="grey lighten-3"
-                  ></v-img>
-                  
-                  {{ reviewBoard.cafeName }}
-
-                  <v-rating
-                    v-model=reviewBoard.rating
-                    bg-color="orange-lighten-1"
-                    color="blue"
+  <div class="main-container">
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="(reviewBoard, index) in calData"
+          :key="index"
+          cols="8"
+          sm="3"
+          md="3"
+          >
+            <v-card class="text-center" :to="{ name: 'ReviewBoardReadPage', params: { reviewBoardId: reviewBoard.reviewBoardId.toString() }}">
+            <v-img
+              :src="reviewBoard.firstPhoto ? getImagePath(reviewBoard.firstPhoto) : ''"
+              aspect-ratio="1"
+              class="grey lighten-3"
+              ></v-img>
+              
+              <v-card-subtitle class="pb-0">{{ reviewBoard.cafeName }}</v-card-subtitle>
+              <v-rating
+                    :value=reviewBoard.rating
+                    color="amber"
+                    dense
+                    half-increments
                     readonly
-                    />
+                    size="14"
+                  ></v-rating>
 
-                  <h3>{{ reviewBoard.title }}</h3>
-                    {{ reviewBoard.writer }}
+              <v-card-text class="text--primary">
+                <div>{{ reviewBoard.title }}</div>
+          
+                <div>
+                  {{reviewBoard.writer }}
+                </div>
+              </v-card-text>
+            </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 
-                  <v-card-actions>
-                    
-                  </v-card-actions>
-                </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </div>
+    <v-pagination
+      v-model="curPageNum"
+      :length="numOfPages"
+      color="#2c4636"
+      class="mt-10"
+      flat
+    ></v-pagination>
+
+  </div>
+
+  
 </template>
 
 <script>
+
 export default {
     name: "ReviewBoardListForm",
     props: {
         reviewBoards: {
             type: Array
         }
+    },
+    data() {
+      return {
+        dataPerPage: 8,
+        curPageNum: 1,
+      };
     },
     methods: {
         getImagePath(imageData) {
@@ -53,9 +73,18 @@ export default {
         },
     },
     computed: {
-    reversedPosts() {
-      return this.reviewBoards.slice().reverse();
-    },
+      startOffset() {
+        return (this.curPageNum - 1) * this.dataPerPage;
+      },
+      endOffset() {
+        return this.startOffset + this.dataPerPage;
+      },
+      numOfPages() {
+        return Math.ceil(this.reviewBoards.length / this.dataPerPage);
+      },
+      calData() {
+        return this.reviewBoards.slice(this.startOffset, this.endOffset);
+      },
   },
 }
 </script>
