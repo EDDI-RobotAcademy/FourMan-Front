4c94927e1ab2c38c6e1de0ccf6c00d249ad26c4d
jmj30040 <jmj30040@gmail.com>
2023-04-12 07:55:47 +0900

    add: [정명진] 마이페이지 내정보 수정시 비밀번호 확인하는 기능 [CAFE-RESERVE-FRONT-68]

--- src/views/memberMyPage/MemberMyPage.vue
+++ src/views/memberMyPage/MemberMyPage.vue
@@ -3,7 +3,7 @@
       <my-page-side-bar-form />
       
       <v-container style="width: 1000px">
-         <member-my-info-form />
+         <member-my-info-form v-if="myInfo" :myInfo="myInfo" />
       </v-container>
    </div>
 </template>
@@ -11,12 +11,26 @@
 <script>
 import MemberMyInfoForm from '@/components/memberMyPage/MemberMyInfoForm.vue'
 import MyPageSideBarForm from '@/components/memberMyPage/MyPageSideBarForm.vue'
+import { mapActions, mapState } from 'vuex'
 
 export default {
    name: "MemberMyPage",
    components: {
       MemberMyInfoForm,
       MyPageSideBarForm
+   },
+   computed: {
+      ...mapState([
+        'myInfo',
+      ]),
+    },
+   created () {
+      this.requestMyInfoToSpring(JSON.parse(localStorage.getItem('userInfo')).id)
+   },
+   methods: {
+      ...mapActions([
+         'requestMyInfoToSpring'
+      ]),
    }
 }
 </script>
