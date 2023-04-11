c2cb80551ae1db2f15bb5a33d45f64cbf6450ea0
jmj30040 <jmj30040@gmail.com>
2023-04-12 07:59:23 +0900

    feat: [정명진] 마이페이지 내정보 수정 UI [CAFE-RESERVE-FRONT-72]

---
+++ src/views/memberMyPage/MemberMyInfoModifyPage.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+       <my-page-side-bar-form />
+       
+       <v-container style="width: 1000px">
+          <member-my-info-modify-form v-if="myInfo" :myInfo="myInfo" @submit="onSubmit"/>
+       </v-container>
+    </div>
+ </template>
+ 
+ <script>
+ import MemberMyInfoModifyForm from '@/components/memberMyPage/MemberMyInfoModifyForm.vue'
+ import MyPageSideBarForm from '@/components/memberMyPage/MyPageSideBarForm.vue'
+import axios from 'axios'
+ import { mapActions, mapState } from 'vuex'
+ 
+ export default {
+    name: "MemberMyInfoModifyPage",
+    components: {
+      MemberMyInfoModifyForm,
+      MyPageSideBarForm
+   },
+   computed: {
+      ...mapState([
+         'myInfo',
+      ]),
+   },
+   created () {
+      this.requestMyInfoToSpring(JSON.parse(localStorage.getItem('userInfo')).id)
+   },
+   methods: {
+      ...mapActions([
+         'requestMyInfoToSpring',
+         'requestMemberInfoModifyToSpring'
+      ]),
+      async onSubmit (payload) {
+         const { memberId, nickName, birthdate, phoneNumber, city, street, addressDetail, zipcode } = payload
+         await this.requestMemberInfoModifyToSpring(
+            { memberId, nickName, birthdate, phoneNumber, city, street, addressDetail, zipcode })
+
+         // 로그아웃
+         let token = localStorage.getItem("userInfo");
+         const length = token.length;
+         token = token.substr(1, length - 2);
+         await axios
+         .post("http://localhost:8888/member/logout", token) //토큰을 보냄
+         .then(() => {
+            localStorage.removeItem("userInfo"); //로컬스토리지에서 제거
+            this.$store.state.isAuthenticated = false;
+         });
+         
+         await this.$router.push({
+            name: 'SignInPage',
+         })
+      },
+   }
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
