e1f40ddedc21fe32946a480586946bc7b73da400
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-04-13 02:22:52 +0900

    feat: [김영진] 카페사업자가 좌석초기화 버튼을 누르면 백엔드로 좌석예약정보 초기화 요청: [CAFE-RESERVE-FRONT-76]

---
+++ src/store/reservation/actions.js
@@ -0,0 +1,37 @@
+import {
+    //예약관련
+    REQUEST_CAFE_SEAT_TO_SPRING,
+
+} from './mutation-types'
+
+// import axios from 'axios'
+import axiosInst from '@/utility/axiosObject'
+export default {
+    //예약 관련
+
+    requestCafeSeatToSpring({commit},payload) {
+        console.log("requestCafeSeatToSpring 작동")
+        const {cafeId,time}=payload
+        return axiosInst.get(`/reservation/cafe/${cafeId}/time/${time}`)
+            .then((res) => {
+                commit(REQUEST_CAFE_SEAT_TO_SPRING, res.data)
+                console.log(" res.data.seatResponse: " + res.data.seatResponse)
+                console.log(" res.data.tableResponse: " + res.data.tableResponse)
+            })
+            .catch((error) => {
+                alert(error)
+            })
+    },
+    requestDeleteCafeSeatToSpring({}) {
+        console.log("requestDeleteCafeSeatToSpring 작동")
+        return axiosInst.delete(`/reservation/seats/delete`)
+            .then(() => {
+                alert("좌석 초기화 성공")
+            })
+            .catch(() => {
+                alert("삭제실패");
+            })
+    },
+
+
+}
