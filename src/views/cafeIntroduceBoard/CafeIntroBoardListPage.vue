54693ea4ad8ebf534147cea7b0cd8a57e4fc8dba
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-04-19 05:48:55 +0900

    fix: [김영진] 카페사업자가 카페등록 후 카페리스트 이미지로딩 에러 수정중

--- src/views/cafeIntroduceBoard/CafeIntroBoardListPage.vue
+++ src/views/cafeIntroduceBoard/CafeIntroBoardListPage.vue
@@ -1,65 +1,85 @@
 <template>
   <v-container>
-   <div class="wrap productWrap">
-    <CafeIntroBoardListForm :cafeLists="cafeLists"></CafeIntroBoardListForm>
-  </div>
+    <div class="wrap productWrap">
+      <CafeIntroBoardListForm :cafeLists="cafeLists"></CafeIntroBoardListForm>
+    </div>
 
-        <div v-if="cafePass == 'CAFE' ">
+    <div v-if="cafePass == 'CAFE'">
       <!-- <router-link :to="{ name: 'CafeIntroBoardRegisterPage' }">
         카페 등록
       </router-link> -->
-      <v-btn type="button" class="brown darken-2 white--text" @click="checkCafeNum" large style="width: 100px; font-size: 18px"
-              >카페 등록
-       </v-btn>
+      <v-btn
+        type="button"
+        class="brown darken-2 white--text"
+        @click="checkCafeNum"
+        large
+        style="width: 100px; font-size: 18px"
+        >카페 등록
+      </v-btn>
     </div>
   </v-container>
 </template>
 
 <script>
-import {mapState, mapActions} from "vuex";
+import store from "@/store";
+import { mapState, mapActions } from "vuex";
 import CafeIntroBoardListForm from "@/components/cafeIntroduceBoard/CafeIntroBoardListForm.vue";
-const cafeIntroduceBoardModule= 'cafeIntroduceBoardModule'
+const cafeIntroduceBoardModule = "cafeIntroduceBoardModule";
 export default {
   name: "CafeIntroBoardListPage",
   components: {
     CafeIntroBoardListForm,
-    }, 
+  },
   computed: {
-    ...mapState(cafeIntroduceBoardModule,[
-      'cafeLists','cafeCheck'
-    ]),
+    ...mapState(cafeIntroduceBoardModule, ["cafeLists", "cafeCheck"]),
   },
   data() {
     return {
-            cafePass: JSON.parse(localStorage.getItem('userInfo')).authorityName,
-            cafeNum:0
-    }
+      cafePass: JSON.parse(localStorage.getItem("userInfo")).authorityName,
+    };
   },
   methods: {
-    ...mapActions(cafeIntroduceBoardModule,['requestCafeListToSpring','requestCafeNumToSpring']),
+    ...mapActions(cafeIntroduceBoardModule, [
+      "requestCafeListToSpring",
+      "requestCafeNumToSpring",
+    ]),
 
-    async checkCafeNum(){
-        if(JSON.parse(localStorage.getItem('userInfo')).authorityName =='CAFE'){
-           await this.requestCafeNumToSpring()
-           await this.changeNum()
+    async checkCafeNum() {
+      if (
+        JSON.parse(localStorage.getItem("userInfo")).authorityName == "CAFE"
+      ) {
+        await this.requestCafeNumToSpring();
+        await this.changeNum();
       }
     },
 
-  changeNum(){
-    if(this.cafeCheck ==true){// this.$store.state.cafeCheck도 가능
-        alert("이미 등록된 카페가 있습니다.")
-    }else{
-      this.$router.push("/cafe-board-register-page");
-    }
-  }
+    changeNum() {
+      if (this.cafeCheck == true) {
+        // this.$store.state.cafeCheck도 가능
+        alert("이미 등록된 카페가 있습니다.");
+      } else {
+        this.$router.push("/cafe-board-register-page");
+      }
+    },
   },
 
-  async mounted() {
-     await this.requestCafeListToSpring()
-   
+  // beforeRouteEnter(to, from, next) {
+  //   store
+  //     .dispatch("cafeIntroduceBoardModule/requestCafeListToSpring")
+  //     .then(() => {
+  //       next();
+  //     });
+  // },
+  // beforeRouteUpdate(to, from, next) {
+  //   this.requestCafeListToSpring().then(() => {
+  //     next();
+  //   });
+  // },
+
+  created() {
+    this.requestCafeListToSpring();
   },
-}
+};
 </script>
 <style scoped>
-
 </style>
