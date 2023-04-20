54ddb6b347398873675fb4dc5483145bacc501d1
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-04-20 15:40:17 +0900

    fix: [김영진] 모든 사용자가 보는 카페리스트 예약시간버튼에서 오늘날짜만 표현 [CAFE-RESERVE-FRONT-85]

--- src/store/reservation/getters.js
+++ src/store/reservation/getters.js
@@ -1,26 +1,24 @@
 export default{
 
     formattedAvailableTimes: (state) => {
-        console.log("formattedAvailableTimes 작동")
+        console.log("formattedAvailableTimes 작동");
         const now = new Date();
-        const tomorrow = new Date(now);
-        tomorrow.setDate(tomorrow.getDate() + 1);
-    
+      
         const formatDate = (date) => {
           return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
         };
-    
+      
         const todayStr = formatDate(now);
-        const tomorrowStr = formatDate(tomorrow);
-        return state.availableTimes.map((time) => {
-            console.log("time",time)
-          const [date, hour] = time.split(" ");
-          if (date === todayStr) {
-            return `Today ${hour}`;
-          } else if (date === tomorrowStr) {
-            return `Tomorrow ${hour}`;
-          }
-          return time;
-        });
+      
+        return state.availableTimes
+          .map((time) => {
+            console.log("time", time);
+            const [date, hour] = time.split(" ");
+            if (date === todayStr) {
+              return `Today ${hour}`;
+            }
+            return null;
+          })
+          .filter((formattedTime) => formattedTime !== null);
       },
 }
