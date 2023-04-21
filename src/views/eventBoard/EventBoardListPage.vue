89b479e6cdaa58b8deb37c00e20a780e56e56a19
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-04-22 05:44:48 +0900

    feat: [김영진] 카페사업자만 이벤트게시판에서 보이는 이벤트등록버튼을 누르면 나오는 이벤트등록페이지 [CAFE-RESERVE-FRONT-103]

---
+++ src/views/eventBoard/EventBoardListPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container fluid>
+    <div>
+      <v-btn
+        type="button"
+        class="brown darken-2 white--text"
+        @click="aa"
+        large
+        style="width: 150px; font-size: 18px"
+        >이벤트 등록
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+const eventBoardModule = "eventBoardModule";
+export default {
+  name: "EventBoardListPage",
+  components: {},
+  computed: {},
+  data() {
+    return { 
+     
+    
+    };
+  },
+  methods: {
+     aa() {
+      this.$router.push("/event-board-register-page");
+      } 
+  },
+};
+</script>
+<style scoped>
+</style>
