6d1bcac58ecf7ded473f5dcacaf7a0dc5574762d
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-04-23 16:00:16 +0900

    style: [김영진] 카페사업자가 카페소개게시판 접속시 보이는 카페등록 버튼과 페이지네이션 같은 줄에 배치 [CAFE-RESERVE-FRONT-35]

--- src/components/cafeIntroduceBoard/CafeIntroBoardListForm.vue
+++ src/components/cafeIntroduceBoard/CafeIntroBoardListForm.vue
@@ -5,34 +5,53 @@
         class="empty_cafe"
         v-if="
           !cafeLists || (Array.isArray(cafeLists) && cafeLists.length === 0)
-        ">
+        "
+      >
         <p>카페가 존재하지 않습니다.</p>
       </div>
       <v-row>
-      <v-col v-for="(cafe, index) in calData" :key="index" cols="12" sm="6" md="4">
-          <CafeIntroBoardCardForm :cafe="cafe"></CafeIntroBoardCardForm >
-
-      </v-col>
+        <v-col
+          v-for="(cafe, index) in calData"
+          :key="index"
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <CafeIntroBoardCardForm :cafe="cafe"></CafeIntroBoardCardForm>
+        </v-col>
       </v-row>
     </v-container>
-
-    <v-pagination
-      v-model="curPageNum"
-      :length="numOfPages"
-      color="brown darken-2"
-      class="mt-10"
-      flat
-    ></v-pagination>
+    <div class="actions">
+      <v-btn
+        v-if="cafePass == 'CAFE'"
+        type="button"
+        class="brown darken-2 white--text"
+        @click="checkCafeNum"
+        large
+        style="width: 100px; font-size: 18px"
+        >카페 등록
+      </v-btn>
+      <div class="pagination-wrapper">
+        <v-pagination
+          v-model="curPageNum"
+          :length="numOfPages"
+          color="brown darken-2"
+          class=""
+          flat
+        ></v-pagination>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import CafeIntroBoardCardForm from "@/components/cafeIntroduceBoard/CafeIntroBoardCardForm.vue";
-
+import { mapState, mapActions } from "vuex";
+const cafeIntroduceBoardModule = "cafeIntroduceBoardModule";
 export default {
   name: "CafeIntroBoardListForm",
   components: {
-    CafeIntroBoardCardForm
+    CafeIntroBoardCardForm,
   },
   props: {
     cafeLists: {
@@ -41,14 +60,34 @@ export default {
   },
   data() {
     return {
+      cafePass: JSON.parse(localStorage.getItem("userInfo")).authorityName,
       dataPerPage: 6,
       curPageNum: 1,
     };
   },
   methods: {
-    
+    ...mapActions(cafeIntroduceBoardModule, ["requestCafeNumToSpring"]),
+
+    async checkCafeNum() {
+      if (
+        JSON.parse(localStorage.getItem("userInfo")).authorityName == "CAFE"
+      ) {
+        await this.requestCafeNumToSpring();
+        await this.changeNum();
+      }
+    },
+
+    changeNum() {
+      if (this.cafeCheck == true) {
+        // this.$store.state.cafeCheck도 가능
+        alert("이미 등록된 카페가 있습니다.");
+      } else {
+        this.$router.push("/cafe-board-register-page");
+      }
+    },
   },
   computed: {
+    ...mapState(cafeIntroduceBoardModule, ["cafeCheck"]),
     startOffset() {
       return (this.curPageNum - 1) * this.dataPerPage;
     },
@@ -64,3 +103,15 @@ export default {
   },
 };
 </script>
+<style scoped>
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.pagination-wrapper {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/views/cafeIntroduceBoard/CafeIntroBoardListPage.vue
+++ src/views/cafeIntroduceBoard/CafeIntroBoardListPage.vue
@@ -4,19 +4,7 @@
       <CafeIntroBoardListForm :cafeLists="cafeLists"></CafeIntroBoardListForm>
     </div>
 
-    <div v-if="cafePass == 'CAFE'">
-      <!-- <router-link :to="{ name: 'CafeIntroBoardRegisterPage' }">
-        카페 등록
-      </router-link> -->
-      <v-btn
-        type="button"
-        class="brown darken-2 white--text"
-        @click="checkCafeNum"
-        large
-        style="width: 100px; font-size: 18px"
-        >카페 등록
-      </v-btn>
-    </div>
+ 
   </v-container>
 </template>
 
@@ -30,50 +18,20 @@ export default {
     CafeIntroBoardListForm,
   },
   computed: {
-    ...mapState(cafeIntroduceBoardModule, ["cafeLists", "cafeCheck"]),
+    ...mapState(cafeIntroduceBoardModule, ["cafeLists"]),
   },
   data() {
     return {
-      cafePass: JSON.parse(localStorage.getItem("userInfo")).authorityName,
+      
     };
   },
   methods: {
     ...mapActions(cafeIntroduceBoardModule, [
       "requestCafeListToSpring",
-      "requestCafeNumToSpring",
     ]),
 
-    async checkCafeNum() {
-      if (
-        JSON.parse(localStorage.getItem("userInfo")).authorityName == "CAFE"
-      ) {
-        await this.requestCafeNumToSpring();
-        await this.changeNum();
-      }
-    },
-
-    changeNum() {
-      if (this.cafeCheck == true) {
-        // this.$store.state.cafeCheck도 가능
-        alert("이미 등록된 카페가 있습니다.");
-      } else {
-        this.$router.push("/cafe-board-register-page");
-      }
-    },
   },
 
-  // beforeRouteEnter(to, from, next) {
-  //   store
-  //     .dispatch("cafeIntroduceBoardModule/requestCafeListToSpring")
-  //     .then(() => {
-  //       next();
-  //     });
-  // },
-  // beforeRouteUpdate(to, from, next) {
-  //   this.requestCafeListToSpring().then(() => {
-  //     next();
-  //   });
-  // },
 
   created() {
     this.requestCafeListToSpring();
