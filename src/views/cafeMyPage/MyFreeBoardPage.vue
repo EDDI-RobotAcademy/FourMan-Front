6ba99e21796e9fdcc2f8797db428a2856826c9ff
Cong0327 <hsp400@gmail.com>
2023-04-24 16:01:34 +0900

    add:[박현수] 마이페이지의 나의게시물, 나의질문, 나의 리뷰에 사이드바 UI 추가로 마이페이지 UI 통일 [CAFE-RESERVE-FRONT-73]

--- src/views/cafeMyPage/MyFreeBoardPage.vue
+++ src/views/cafeMyPage/MyFreeBoardPage.vue
@@ -1,20 +1,25 @@
 <template>
+  <div>
+    <my-page-side-bar-form/>
     <v-container>
       <div class="EULJIRO mt-5 mb-5 text-center">
         <h1>내가 쓴 게시물 <v-icon>mdi-bulletin-board</v-icon></h1>
     </div>
         <my-free-board-form :myFreeBoards="myFreeBoards"/>
     </v-container>
+    </div>
   </template>
 
   <script>
 
   import MyFreeBoardForm from '@/components/cafeMyPage/MyFreeBoardForm.vue'
   import { mapActions, mapState } from 'vuex'
+  import MyPageSideBarForm from '@/components/memberMyPage/MyPageSideBarForm.vue'
+
   const myPageModule = 'myPageModule'
 
   export default {
-    components: { MyFreeBoardForm },
+    components: { MyFreeBoardForm, MyPageSideBarForm },
     name: "MyFreeBoardPage",
     computed: {
       ...mapState(myPageModule,[
