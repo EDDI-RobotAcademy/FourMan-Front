18963299165e1e6aaba718531785b678007d03ba
jmj30040 <jmj30040@gmail.com>
2023-04-29 03:07:20 +0900

    fix: [정명진] 일반회원이 마이페이지 내가쓴 자유게시물 페이지에서 기존의 자유게시판 컴포넌트를 사용하도록 수정 [CAFE-RESERVE-FRONT-67]

--- src/views/cafeMyPage/MyFreeBoardPage.vue
+++ src/views/cafeMyPage/MyFreeBoardPage.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="d-flex">
     <my-page-side-bar-form/>
-    <v-container>
-      <div class="EULJIRO mt-5 mb-5 text-center">
-        <h1>내가 쓴 게시물 <v-icon>mdi-bulletin-board</v-icon></h1>
+    <v-container class="ms-16 me-16">
+      <div class="NanumGothic mt-10 mb-4">
+        <h2>내가 쓴 게시물 <v-icon>mdi-bulletin-board</v-icon></h2>
     </div>
-        <my-free-board-form :myFreeBoards="myFreeBoards"/>
+      <free-board-list-form
+            :freeBoards="myFreeBoards" />
+        <!-- <my-free-board-form :myFreeBoards="myFreeBoards"/> -->
     </v-container>
     </div>
   </template>
 
   <script>
 
-  import MyFreeBoardForm from '@/components/cafeMyPage/MyFreeBoardForm.vue'
+  import FreeBoardListForm from '@/components/freeBoard/FreeBoardListForm.vue'
+  // import MyFreeBoardForm from '@/components/cafeMyPage/MyFreeBoardForm.vue'
   import { mapActions, mapState } from 'vuex'
   import MyPageSideBarForm from '@/components/memberMyPage/MyPageSideBarForm.vue'
 
   const myPageModule = 'myPageModule'
 
   export default {
-    components: { MyFreeBoardForm, MyPageSideBarForm },
+    components: { MyPageSideBarForm, FreeBoardListForm },
     name: "MyFreeBoardPage",
     computed: {
       ...mapState(myPageModule,[
